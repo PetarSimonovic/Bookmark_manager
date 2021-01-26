7b49c829709236ad11433667b6352c9688336a10
PetarSimonovic <petar@simonovic.com>
2021-01-26 15:47:32 +0000

    Replaced hard-coded database with Postgres

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,9 +5,9 @@ describe Bookmark do
     it 'contains some url bookmarks' do
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include("https://www.youtube.com")
-      expect(bookmarks).to include("https://www.google.co.uk")
-      expect(bookmarks).to include("https://www.bbc.co.uk")
+      expect(bookmarks).to include("http://www.youtube.com")
+      expect(bookmarks).to include("http://www.google.co.uk")
+      expect(bookmarks).to include("http://www.bbc.co.uk")
     end
   end
 end
