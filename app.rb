099f8fe7834efa354dc91a89af50628719058742
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-29 11:58:26 +0000

    Update step feature test failing

--- app.rb
+++ app.rb
@@ -24,6 +24,17 @@ class BookmarkManager < Sinatra::Base
     redirect '/'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark_id = params[:id]
+    erb(:edit)
+  end
+
+  patch 'bookmarks/:id' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("UPDATE bookmarks SET url = '#{params[:new_bookmark]}', title = '#{params[:title]}' WHERE id = '#{params[:id]}'")
+    redirect'/'
+  end
+
   run! if app_file == $0
 
 end
